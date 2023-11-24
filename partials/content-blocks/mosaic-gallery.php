157e29d7a724efcdddf952963a3f0c6d70401842
geomarts <netdark@hotmail.gr>
2023-11-24 18:10:55 +0200

    start gallery component

--- partials/content-blocks/mosaic-gallery.php
+++ partials/content-blocks/mosaic-gallery.php
@@ -1,7 +1,41 @@
-<?php
-?>
 
 <section class="section section-mosaic-gallery">
+	<ul class="grid list-base">
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-1.jpg" alt="">
+			</figure>
+		</li>
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-2.jpg" alt="">
+			</figure>
+		</li>
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-3.jpg" alt="">
+			</figure>
+		</li>
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-4.jpg" alt="">
+			</figure>
+		</li>
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-5.jpg" alt="">
+			</figure>
+		</li>
+		<li>
+			<figure class="uk-margin-remove">
+				<img class="object-fit-cover img1 uk-width-1-1" src="http://localhost/avax/wp-content/uploads/2023/11/mosaic-6.jpg" alt="">
+			</figure>
+		</li>
+	</ul>
+</section>
+
+
+<?php /*<section class="section section-mosaic-gallery">
 	<div class="uk-grid-collapse" uk-grid>
 		<div class="uk-width-1-2 uk-flex uk-flex-column">
 			<a class="uk-width-1-1 uk-flex" href="">
@@ -43,4 +77,4 @@
 		</div>
 
 	</div>
-</section>
+</section>*/ ?>
